07cb65b3d00d3ea5f78e9f4de554603eb7d445a9
Tamar <t4165125@gmail.com>
2024-03-03 23:29:10 +0200

    add class UpdateAgedBrie implements IUpdateItems and function updateQuality  implementation.

---
+++ src/UpdateAgedBrie.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose;
+
+class UpdateAgedBrie implements IUpdateItems
+{
+    public function updateQuality(Item $item): void{
+        if ($item->sellIn < 0) {
+            $item->quality = $item->quality + 1;
+        }
+        $item->quality = $item->quality + 1;
+        if ($item->quality > 50) {
+            $item->quality = 50;
+        }
+    }
+}
