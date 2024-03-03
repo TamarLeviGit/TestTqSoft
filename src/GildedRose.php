251c7a151990cea2b350b804b49dc27c87937ee8
Tamar <t4165125@gmail.com>
2024-03-03 22:52:50 +0200

    Adding a 'Conjured Mana Cake' to a function updateQuality()

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -45,6 +45,15 @@ final class GildedRose
                         }
                     }
                 }
+                else if($item->name == 'Conjured Mana Cake'){
+                    if($item->sellIn < 0) {
+                        $item->quality = $item->quality - 2;
+                    } 
+                    $item->quality = $item->quality - 2;
+                    if($item->quality < 0){
+                        $item->quality = 0;
+                    }
+                }
                 else{
                     if($item->sellIn < 0) {
                         $item->quality = $item->quality - 1;
