719be90bab1eceb942717e33ce9fb4d004084a21
Tamar <t4165125@gmail.com>
2024-03-03 23:33:30 +0200

    add class UpdateBackstagePasses implements IUpdateItems and updateQuality function

---
+++ src/UpdateBackstagePasses.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose;
+
+class UpdateBackstagePasses implements IUpdateItems
+{
+    public function updateQuality(Item $item): void{
+        if($item->sellIn < 0) {
+            $item->quality = 0;
+        }
+        else{
+            $item->quality = $item->quality + 1;
+            if ($item->sellIn < 5) {
+                $item->quality = $item->quality + 2;
+            }
+            else if ($item->sellIn < 10) {
+                $item->quality = $item->quality + 1;
+            }
+            if ($item->quality > 50) {
+                $item->quality = 50;
+            }
+        }
+    }
+}
