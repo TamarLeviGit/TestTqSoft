7d8aeb717b02a1ec42997a5977b04c523942d5ae
Tamar <t4165125@gmail.com>
2024-03-03 19:42:04 +0200

    Change 2 tests greater than fifty to one test that will update to fifty if exceeded.

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -18,45 +18,44 @@ final class GildedRose
     {
         foreach ($this->items as $item) {
             if ($item->name != 'Sulfuras, Hand of Ragnaros') { 
-
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    $item->quality = $item->quality - 1;     
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
+                if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->quality > 0) {
+                        $item->quality = $item->quality - 1;     
                     }
-                }
-            }
-            $item->sellIn = $item->sellIn - 1;
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            $item->quality = $item->quality - 1;
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
+                } 
+                else {
                     if ($item->quality < 50) {
                         $item->quality = $item->quality + 1;
+                        if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                            if ($item->sellIn < 11) {
+                                $item->quality = $item->quality + 1;
+                            }
+                            if ($item->sellIn < 6) {
+                                $item->quality = $item->quality + 1;
+                            }
+                            if ($item->quality > 50) {
+                                $item->quality = 50;
+                            }
+                        }
                     }
                 }
+                $item->sellIn = $item->sellIn - 1;
+                if ($item->sellIn < 0) {
+                    if ($item->name != 'Aged Brie') {
+                        if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                            if ($item->quality > 0) {
+                                $item->quality = $item->quality - 1;
+                            }
+                        } else {
+                            $item->quality = $item->quality - $item->quality;
+                        }
+                    } else {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
+                        }
+                    }
+                }  
             }
         }
-    }
     }
 }
